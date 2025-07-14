fe24122ba949a1812ab633698503866cec33e59f
irupp <irupp@my.waketech.edu>
2025-07-14 13:05:02 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,28 @@
+// Function to fetch student data from the database
+function fetchStudents() {
+    // TODO: Implement database call to get student names
+    console.log("Fetching students...");
+}
+
+// Function to fetch assignment data from the database
+function fetchAssignments() {
+    // TODO: Implement database call to get assignment names
+    console.log("Fetching assignments...");
+}
+
+// Function to fetch grades for all students and assignments
+function fetchGrades() {
+    // TODO: Implement database call to get student grades
+    console.log("Fetching grades...");
+}
+
+// Function to populate the gradebook table in HTML
+function populateGradebook() {
+    // TODO: Call fetch functions and update the DOM with data
+    console.log("Populating gradebook...");
+}
+
+// Call this function when the page loads
+window.onload = function () {
+    populateGradebook();
+};
